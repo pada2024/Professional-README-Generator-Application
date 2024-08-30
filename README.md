216f9664db89178ecb14d9ba550186d10daa0433
Parisa Daily <misssweetardor@yahoo.com>
2024-08-29 22:12:44 -0700

    uncommented my other prompts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# p 
+# Parisa 
 
 ## Description
 undefined
--- index.js
+++ index.js
@@ -11,55 +11,55 @@ inquirer.prompt([
         message: 'What is your name?',
     },
 
-    // {
-    //     type: 'input',
-    //     name: 'Title',
-    //     message: 'What is the name of your app?',
-    // },
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the name of your app?',
+    },
 
 
-    // {
-    //     type: 'input',
-    //     name: 'description',
-    //     message: 'Please descrie your application?',
-    // },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please descrie your application?',
+    },
 
-    // {
-    //     type: 'input',
-    //     name: 'motivation',
-    //     message: 'What is your motivation behind building this application?',
-    // },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What is your motivation behind building this application?',
+    },
 
-    // {
-    //     type: 'input',
-    //     name: 'build',
-    //     message: 'Why did you build this applicaton and what is it for',
-    // },
+    {
+        type: 'input',
+        name: 'build',
+        message: 'Why did you build this applicaton and what is it for',
+    },
 
-    // {
-    //     type: 'input',
-    //     name: 'resolution',
-    //     message: 'How do you use your app and What problem does your application solve?',
-    // },
+    {
+        type: 'input',
+        name: 'resolution',
+        message: 'How do you use your app and What problem does your application solve?',
+    },
 
 
-    // {
-    //     type: 'input',
-    //     name: 'educationn',
-    //     message: 'What did you learn while creating your application?',
-    // },
+    {
+        type: 'input',
+        name: 'educationn',
+        message: 'What did you learn while creating your application?',
+    },
 
-    // {
-    //     type: 'input',
-    //     name: 'bugs',
-    //     message: 'How do you report issues with your app?',
-    // },
+    {
+        type: 'input',
+        name: 'bugs',
+        message: 'How do you report issues with your app?',
+    },
 
-    // {
-    //     type: 'input',
-    //     name: 'contribute',
-    //     message: 'How can one contribute to this app?',
-    // },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can one contribute to this app?',
+    },
 
     {
         type: 'list',
